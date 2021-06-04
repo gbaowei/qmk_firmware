9cb7182aa5d7375defada5bb214c030537f9f0f1
gbaowei <67459515+gbaowei@users.noreply.github.com>
2021-06-04 11:39:14 +0800

    Update keymap.c

--- keyboards/kawaii/keymaps/bao/keymap.c
+++ keyboards/kawaii/keymaps/bao/keymap.c
@@ -15,53 +15,51 @@
  */
 #include QMK_KEYBOARD_H
 
-enum layer_number {
-    _BASE,
-    _LOWER,
-    _RAISE,
-    _ADJUST,
-};
-
-enum custom_keycodes {
-    LOWER = SAFE_RANGE,
-    RAISE,
-    KANJI,
+enum layers {
+  _BASE,
+  _NUM_SYM,
+  _NAV,
+  _ADJUST,
 };
 
 #define KC_AJST  MO(_ADJUST)
 
-// Base layer mod tap
 enum combo_events {
   COMBO_BSPC,
   COMBO_NUMBAK,
   COMBO_TAB,
   COMBO_ESC,
   COMBO_DEL,
-  COMBO_QUOT
+  COMBO_QUOT,
+  COMBO_SCLN,
+  COMBO_SLSH
 };
 
-#define KC_LSPC LT(_LOWER, KC_SPC)
-#define KC_RENT LT(_RAISE, KC_ENT)
+#define KC_LSPC LT(_NUM_SYM, KC_SPC)
+#define KC_RENT LT(_NAV, KC_ENT)
+
 #define KC_GA LGUI_T(KC_A)
 #define KC_AS LALT_T(KC_S)
 #define KC_CD LCTL_T(KC_D)
 #define KC_SF LSFT_T(KC_F)
+
+#define KC_GH RGUI_T(KC_H)
 #define KC_SJ RSFT_T(KC_J)
 #define KC_CK RCTL_T(KC_K)
 #define KC_AL RALT_T(KC_L)
-#define KC_GH RGUI_T(KC_H)
 
-// Lower layer mod tap
-#define KC_F1AL  LALT_T(KC_F1)
-#define KC_F6CT  LCTL_T(KC_F6)
-#define KC_11SF  LSFT_T(KC_F11)
 
-#define KC_QUSF  RCTL_T(KC_QUOT)
-#define KC_ROSF  RSFT_T(KC_RO)
+// // Lower layer mod tap
+// #define KC_F1AL  LALT_T(KC_F1)
+// #define KC_F6CT  LCTL_T(KC_F6)
+// #define KC_11SF  LSFT_T(KC_F11)
 
-// Layer tap
-#define KC_BSLO  LT(_LOWER, KC_BSPC)
-#define KC_SPRA  LT(_RAISE, KC_SPC)
+// #define KC_QUSF  RCTL_T(KC_QUOT)
+// #define KC_ROSF  RSFT_T(KC_RO)
+
+// // Layer tap
+// #define KC_BSLO  LT(_LOWER, KC_BSPC)
+// #define KC_SPRA  LT(_RAISE, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
@@ -72,29 +70,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,      KC_DOT,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
-      _______, KC_LALT, KC_LGUI,          KC_LSPC,          KC_RENT,  KC_ENT, KC_AJST, _______
+      _______, KC_LCTL, KC_LALT,     KC_LSPC,               KC_RENT, KC_ENT, KC_AJST, _______
   //`-----------------------------------------------------------------------------------------'
   ),
 
-  [_LOWER] = LAYOUT(
+  [_NUM_SYM] = LAYOUT(
   //,-----------------------------------------------------------------------------------------.
-      KC_F1AL,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
+      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
-      KC_F6CT,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX, XXXXXXX, KC_SCLN,     KC_QUSF,
+      KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,     KC_MINS,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
-      KC_11SF,  KC_F12,  KC_ESC,  KC_TAB,   KANJI,   KC_RO,  KC_GRV, KC_SLSH,     KC_ROSF,
+      KC_BSLS, KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS, KC_RPRN, KC_RBRC, KC_RCBR,     KC_EQL,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
-      _______, _______, _______,          KC_AJST,          KC_AJST, _______, _______, _______
+      _______, KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, _______, _______, _______
   //`-----------------------------------------------------------------------------------------'
   ),
 
-  [_RAISE] = LAYOUT(
+  [_NAV] = LAYOUT(
   //,-----------------------------------------------------------------------------------------.
-         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
       KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP,     KC_RGHT,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_DEL, KC_COMM,  KC_DOT,     KC_BSLS,
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_DEL, KC_COMM,  KC_DOT,     RESET,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
       _______, _______, _______,          _______,          _______, _______, _______, _______
   //`-----------------------------------------------------------------------------------------'
@@ -118,8 +116,10 @@ const uint16_t PROGMEM combo_bspc[] = {KC_O, KC_P, COMBO_END};
 const uint16_t PROGMEM combo_numbak[] = {KC_0, KC_9, COMBO_END};
 const uint16_t PROGMEM combo_tab[] = {KC_Q, KC_W, COMBO_END};
 const uint16_t PROGMEM combo_esc[] = {KC_Q, KC_E, COMBO_END};
-const uint16_t PROGMEM combo_del[] = {KC_MINS, KC_EQL, COMBO_END};
+const uint16_t PROGMEM combo_del[] = {KC_ASTR, KC_MINS, COMBO_END};
 const uint16_t PROGMEM combo_quot[] = {KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM combo_scln[] = {KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM combo_slsh[] = {KC_COMM, KC_DOT, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   [COMBO_BSPC] = COMBO(combo_bspc,KC_BSPC),
@@ -127,10 +127,23 @@ combo_t key_combos[COMBO_COUNT] = {
   [COMBO_TAB] = COMBO(combo_tab,KC_TAB),
   [COMBO_ESC] = COMBO(combo_esc,KC_ESC),
   [COMBO_DEL] = COMBO(combo_del,KC_DEL),
-  [COMBO_QUOT] = COMBO(combo_quot, KC_QUOT)
+  [COMBO_QUOT] = COMBO(combo_quot, KC_QUOT),
+  [COMBO_SCLN] = COMBO(combo_scln, KC_SCLN),
+  [COMBO_SLSH] = COMBO(combo_slsh, KC_SLSH)
 
 };
 #endif
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_LSPC:
+            return 150;
+        case KC_RENT:
+            return 150;
+        default:
+            return TAPPING_TERM;
+    }
+ }
 
 // uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 //   switch (keycode) {
