3319dec1d9ca10842ee9075bd6df4ab675207693
gbaowei <67459515+gbaowei@users.noreply.github.com>
2021-07-19 14:17:33 +0800

    Update keymap.c

--- keyboards/kawaii/keymaps/bao/keymap.c
+++ keyboards/kawaii/keymaps/bao/keymap.c
@@ -19,7 +19,7 @@ enum layers {
   _BASE,
   _LETSGO,
   _BOIS,
-  _ADJUST,
+  _MAUS
 };
 
 enum combo_events {
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,      KC_DOT,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
-      _______, KC_LCTL, KC_LALT,     KC_NSPC,               KC_NENT, KC_ENT, KC_AJST, _______
+      _______, KC_LCTL, KC_LALT,     KC_NSPC,               KC_NENT, KC_ENT, MO(_MAUS), _______
   //`-----------------------------------------------------------------------------------------'
   ),
 
@@ -87,25 +87,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BOIS] = LAYOUT(
   //,-----------------------------------------------------------------------------------------.
-       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_HOME, KC_PGDN, KC_UP,   KC_PGUP, KC_END,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP,     KC_RGHT,
+      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_GRV,  KC_LEFT, KC_DOWN,     KC_RGHT,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_DEL, KC_COMM,  KC_DOT,     RESET,
+      KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX,  KC_DEL,  KC_COMM, KC_DOT,      RESET,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
-      _______, _______, _______,          _______,          _______, _______, _______, _______
+      _______, KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, _______, _______, _______
   //`-----------------------------------------------------------------------------------------'
   ),
 
-  [_ADJUST] = LAYOUT(
+  [_MAUS] = LAYOUT(
   //,-----------------------------------------------------------------------------------------.
-        RESET, XXXXXXX, AG_NORM, AG_SWAP, KC_PSCR, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  KC_INS,
+      KC_WH_L, KC_WH_R, KC_MS_U, XXXXXXX, KC_WH_U, KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, KC_NLCK, KC_MS_L, KC_MS_D, KC_MS_U,     KC_MS_R,
+      KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_SLCK, XXXXXXX, XXXXXXX,     XXXXXXX,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_CAPS, KC_BTN1, KC_BTN2, XXXXXXX,     XXXXXXX,
+      XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, KC_NLCK, XXXXXXX, XXXXXXX,     XXXXXXX,
   //|--------+--------+--------+--------+--------|--------+--------+--------+--------+--------|
-      _______, _______, _______,          _______,          _______, _______, _______, _______
+      XXXXXXX, XXXXXXX, KC_BTN3,          KC_BTN1,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
   //`-----------------------------------------------------------------------------------------'
   )
 };
